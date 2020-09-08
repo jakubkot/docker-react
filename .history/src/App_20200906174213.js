5f0ec04b14f44ebf8c71b7e0560d90dee8981b47
Jakub Kot <jakub.mikolaj.kot@gmail.com>
2020-09-08 19:41:03 +0200

    initial commit

--- .history/src/App_20200906174213.js
+++ .history/src/App_20200906174213.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there! 333
+          Hi there! 222
         </p>
         <a
           className="App-link"
